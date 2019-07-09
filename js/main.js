bc2928d4ccd25658e8824cd914f45e2de1c14857
y.harata <>
2019-07-10 06:40:13 +0900

    レイアウトを微調整

--- js/main.js
+++ js/main.js
@@ -36,72 +36,34 @@ $(function() {
     ]
   });
 
-  //prevボタン、nextボタンの表示非表示
-  function toggleChangeBtn() {
-    var slideIndex = $('.slide').index($('.active'));
-    $('.change-btn').show();
-    if (slideIndex == 0) {
-      $('.prev-btn').hide();
-    } else if (slideIndex == $('.slide').length - 1) {
-      $('.next-btn').hide();
-    }
-  }
-
-  // インデックスボタン押下時
-  $('.index-btn').click(function() {
-    $('.active').removeClass('active');
-
-    var clickedIndex = $('.index-btn').index($(this));
-    $('.slide').eq(clickedIndex).addClass('active');
-
-    toggleChangeBtn();
-  });
-
-  // prevボタン、nextボタン押下時
-  $('.change-btn').click(function() {
-    // 現在表示されているスライドを変数に退避する
-    var $displaySlide = $('.active');
-
-    $displaySlide.removeClass('active');
-
-    if ($(this).hasClass('next-btn')) {
-      $displaySlide.next().addClass('active');
-    } else {
-      $displaySlide.prev().addClass('active');
-    }
-    toggleChangeBtn();
-  });
+  //スクロール
+  $('#introduction-header').on('click', function() {
+      $("html,body").animate({scrollTop:$('.introduction').offset().top-70});
+  })
+  $('#products-header').on('click', function() {
+      $("html,body").animate({scrollTop:$('.products').offset().top-70});
+  })
+  $('#contact-header').on('click', function() {
+      $("html,body").animate({scrollTop:$('.contact').offset().top-70});
+  })
 
   //topへ戻るボタン
   $('#back-to-top').hide();
   //スクロールが十分されたら表示、スクロールが戻ったら非表示
   $(window).scroll(function() {
-    if ($(this).scrollTop() > 80) {
+    if ($(this).scrollTop() > 300) {
       $('#back-to-top').fadeIn();
     } else {
       $('#back-to-top').fadeOut();
     }
   });
   //クリック時topへ戻る
-  $('#back-to-top a').click(function() {
-    $('body').animate({
+  $('#back-to-top').click(function() {
+    $('html,body').animate({
       scrollTop:100
-    }, 800);
+    }, 400);
     return false;
 
-  });
-
-  //introductionへ遷移
-  //introducitonの位置を取得
-  var introductionLine = document.getElementById("introduction");
-  //取得した位置へ遷移
-  $('#introduction-header').click(function(){
-    console.log(introductionLine);
-    $('body').animate({
-      scrollTop:introductionLine
-    }, 500);
-    return false;
-  });
-
+  })
 
 });
